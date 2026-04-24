c8302bd45875871eacaef1c0fc2b9df16ab4dc6d
optmsshiv <optmsshiv@gmail.com>
2026-04-24 11:25:03 +0530

    pth update

--- api/test2.php
+++ api/test2.php
@@ -1,6 +1,18 @@
 <?php
-echo __DIR__;
-echo "<br>";
-echo dirname(__DIR__);
-echo "<br>";
-echo file_exists(dirname(__DIR__) . '/vendor/autoload.php') ? 'FOUND' : 'NOT FOUND';
+require_once '/home1/edrppymy/public_html/invoiceoptms/vendor/autoload.php';
+
+echo "Autoload OK<br>";
+
+if (class_exists('\Mpdf\Mpdf')) {
+    echo "mPDF class FOUND ✅";
+} else {
+    echo "mPDF class NOT FOUND ❌<br>";
+    
+    // Show what mpdf files actually exist
+    $path = '/home1/edrppymy/public_html/invoiceoptms/vendor/mpdf';
+    echo "Files in vendor/mpdf/:<br>";
+    foreach (scandir($path) as $f) {
+        echo $f . "<br>";
+    }
+}
+?>
