7b7c3fbade24a87fa6621fd20d0de619d15e7ec4
optmsshiv <optmsshiv@gmail.com>
2026-06-17 11:26:24 +0530

    chartbar height changed

--- index.php
+++ index.php
@@ -332,7 +332,7 @@ canvas { max-width: 100% !important; }
 .dash-chart-card canvas,
 .dash-calendar-card canvas { display: block; }
 .dash-chart-card .chart-wrap,
-.reports-chart-wrap { position: relative; height: 200px; overflow: hidden; }
+.reports-chart-wrap { position: relative; height: 300px; overflow: hidden; }
 .reports-chart-wrap-lg { position: relative; height: 240px; overflow: hidden; }
 .card-header {
   display: flex; align-items: center; justify-content: space-between;
